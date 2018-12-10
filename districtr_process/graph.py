223862d173aa7d0c340740f126243d48c2020319
Max Hully <max.hully@gmail.com>
2018-12-09 21:13:58 -0500

    Add ma_towns, PA

--- districtr_process/graph.py
+++ districtr_process/graph.py
@@ -2,5 +2,14 @@ from gerrychain import Graph
 
 
 def save_graph(place, filename):
-    graph = Graph.from_file(filename, columns=[col.key for col in place.columns])
-    graph.to_json("./graphs/{}.json".format(place.id))
+    print("Creating rook graph")
+    rook = Graph.from_file(
+        filename, adjacency="rook", columns=[col.key for col in place.columns]
+    )
+    rook.to_json("./graphs/{}_rook.json".format(place.id))
+
+    print("Creating queen graph")
+    queen = Graph.from_file(
+        filename, adjacency="queen", columns=[col.key for col in place.columns]
+    )
+    queen.to_json("./graphs/{}_queen.json".format(place.id))
